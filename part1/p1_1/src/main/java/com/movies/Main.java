63606e34fbe3c618be0783233347649ee04257f0
Alyona-Soboleva <alena.soboleva.2006@mail.ru>
2026-05-22 19:24:45 +0300

    Completed task 1_1

--- part1/p1_1/src/main/java/com/movies/Main.java
+++ part1/p1_1/src/main/java/com/movies/Main.java
@@ -3,14 +3,14 @@
  * Учебное задание: Maven - первая программа
  */
 public class Main {
-    
+
     /**
      * TODO: Выведите сообщение "Movie App Started" в консоль
      * Используйте System.out.println()
      */
     public static void main(String[] args) {
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        
+        System.out.println("Movie App Started");
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 }
